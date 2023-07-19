8134a57de5b6d964119aacc1c71ca2478cb80c09
Martin Villa <martinvilla027@gmail.com>
2023-07-19 14:09:27 -0500

    first commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
+
+@Component({
+    //We deleted the selector property, we'll instead diplay this component's view as part of the routing
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+    pageTitle: string = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    errorMessage: string = "";
+    // bang or exclamation point, tells TypeScript compiler we will handle
+    // the assigment of this property sometime later (ngOnInit)
+    sub!: Subscription; 
+
+    private _listFilter: string = '';
+
+    get listFilter(): string{
+        return this._listFilter;
+    }
+
+    set listFilter(value: string){
+        this._listFilter = value;
+        console.log('In setter:', value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService){
+
+    }
+  
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+           next: products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+            },
+           error: err => this.errorMessage = err
+        });
+        
+        console.log("In OnInit");
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = "Product List " + message;
+    }
+}
